098b9704a143f2ce6750083eb83b1a13dc3596a8
Fluqie <14fragnatic88@gmail.com>
2020-12-16 20:50:07 +0300

    last comm

--- src/scraping_service/urls.py
+++ src/scraping_service/urls.py
@@ -1,7 +1,6 @@
 """scraping_service URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,11 +13,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import home
+from django.urls import path, include
+from scraping.views import (
+    home_view, list_view, v_detail, VDetail, VList, VCreate, VUpdate, VDelete
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
-
+    path('list/', list_view, name='list'),
+    # path('list/', VList.as_view(), name='list'),
+    path('accounts/', include(('accounts.urls', 'accounts'))),
+    path('detail/<int:pk>/', VDetail.as_view(), name='detail'),
+    path('create/', VCreate.as_view(), name='create'),
+    path('update/<int:pk>/', VUpdate.as_view(), name='update'),
+    path('delete/<int:pk>/', VDelete.as_view(), name='delete'),
+    path('', home_view, name='home'),
 ]
--- src/scraping_service/views.py
+++ src/scraping_service/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 import datetime
 
-def home (request):
+
+def home(request):
     date = datetime.datetime.now().date()
     name = 'Dave'
     _context = {'date': date, 'name': name}
